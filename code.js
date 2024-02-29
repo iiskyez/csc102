5f69d2f982971e04fb76c776ea17529b40df052b
skye <156351836+iiskyez@users.noreply.github.com>
2024-02-29 10:47:19 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -54,3 +54,49 @@ function rollDie(){
     // we will get a rounded whole number that will be returned to whatever code calls this function
     return Math.ceil(die);
 }
+
+// this function will validate the form input to make sure it meets our criteria
+function validateForm(){
+    // get the first name that the user entered on the form
+    var firstName = document.getElementById("fname").value;
+
+    // get the last name that the user entered on the form
+    var lastName = document.getElementById("lname").value;
+
+    // get the zip code that the user entered on the form
+    var zipCode = document.getElementById("zip").value;
+
+    console.log("First Name: " + firstName);
+    console.log("Last Name: " + lastName);
+    console.log("Zip Code: " + zipCode);
+
+    // create a variable to hold firstName + space + lastName
+    var fullName = firstName + " " + lastName;
+    console.log("Full Name: " + fullName);
+
+    // do the input validation
+    // we will check for more than 20 letters in the  first and last names
+    // or if no names were entered, the length will be 1 because of the space we add between first and last name
+    if(fullName.length > 20 || fullName.length == 1){
+        console.log("Invalid Name");
+        // let the user know that they did not enter the information in correctly
+        alert("Please enter a name that is shorter than 20 letters.");
+        // make sure the form does not submit
+        return false;
+    }
+    else if (zipCode.length != 5){
+        console.log("Invalid Zip Code");
+        // let the user know that the zip code was not entered correctly
+        alert("Zip code was not 5 digits. Please try again.");
+        // make sure the form does not submit
+        return false;
+    }
+    else{
+        // they passed validation! release the secret word!
+        console.log("We are good to go.");
+        alert("Welcome, " + fullName + ". The secret word is validation.");
+        return false;
+    }
+
+    return false;
+}
